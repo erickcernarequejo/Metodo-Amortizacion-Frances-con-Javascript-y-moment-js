80f9715fac75597b709b86c9531ae2cd338aa6b8
Erick Edgardo Cerna Requejo <william8178@hotmail.com>
2020-08-15 19:01:04 -0500

    Calcular cronograma de pagos

---
+++ js/amortizacion.js
@@ -0,0 +1,50 @@
+
+const monto = document.getElementById('monto');
+const tiempo = document.getElementById('tiempo');
+const interes = document.getElementById('interes');
+const btnCalcular = document.getElementById('btnCalcular');
+const llenarTabla = document.querySelector('#lista-tabla tbody');
+
+btnCalcular.addEventListener('click', () => {
+    calcularCuota(monto.value, interes.value, tiempo.value);
+})
+
+function calcularCuota(monto, interes, tiempo){
+
+    while(llenarTabla.firstChild){
+        llenarTabla.removeChild(llenarTabla.firstChild);
+    }
+
+    let fechas = [];
+    let fechaActual = Date.now();
+    let mes_actual = moment(fechaActual);
+    mes_actual.add(1, 'month');    
+
+    let pagoInteres=0, pagoCapital = 0, cuota = 0;
+
+    cuota = monto * (Math.pow(1+interes/100, tiempo)*interes/100)/(Math.pow(1+interes/100, tiempo)-1);
+
+    for(let i = 1; i <= tiempo; i++) {
+
+        pagoInteres = parseFloat(monto*(interes/100));
+        pagoCapital = cuota - pagoInteres;
+        monto = parseFloat(monto-pagoCapital);
+
+        //Formato fechas
+        fechas[i] = mes_actual.format('DD-MM-YYYY');
+        mes_actual.add(1, 'month');
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${fechas[i]}</td>
+            <td>${cuota.toFixed(2)}</td>
+            <td>${pagoCapital.toFixed(2)}</td>
+            <td>${pagoInteres.toFixed(2)}</td>
+            <td>${monto.toFixed(2)}</td>
+        `;
+        llenarTabla.appendChild(row)
+    }
+}
+
+
+
